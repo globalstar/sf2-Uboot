cd8203e55190fdcef1d044699ce50d20dbe7fb8c
Aaron Clarke <ac@aaronclarke.com>
2017-09-11 13:28:41 -0700

    Testing pinmux to debug eMMC intermittant failure.

--- board/ti/am335x/mux.c
+++ board/ti/am335x/mux.c
@@ -392,8 +392,8 @@ void enable_board_pin_mux(void)
 #endif
 	} else if (board_is_sf2()) {
 		/* Sat-Fi 2 pinmux */
-		/* configure_module_pin_mux(mmc1_pin_mux);   */  /* ACDEBUG */
 		configure_module_pin_mux(nand_pin_mux);
+		configure_module_pin_mux(mmc1_pin_mux);
 	} else if (board_is_icev2()) {
 		configure_module_pin_mux(mmc0_pin_mux);
 		configure_module_pin_mux(gpio0_18_pin_mux);
